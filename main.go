3976d2bfc9bde18df7765f67d25173ffd85c7c2f
lifangjun <lifj@ebondhm.com>
2022-09-05 11:03:20 +0800

    feature: add usage print info

--- main.go
+++ main.go
@@ -12,8 +12,10 @@ var (
 	client   *oss.Client
 	err      error
 	filePath string
+	help     bool = false
 )
 
+// init
 func init() {
 	client, err = oss.New(conf.OssEndPoint, conf.OssAccessKeyId, conf.OssAccessSecret)
 	if err != nil {
@@ -22,6 +24,7 @@ func init() {
 	}
 }
 
+// validate
 func validate() error {
 	if conf.OssAccessSecret == "" || conf.OssAccessKeyId == "" || conf.OssAccessSecret == "" {
 		return fmt.Errorf("oss connection params is empty")
@@ -32,17 +35,47 @@ func validate() error {
 	return nil
 }
 
+// loadParams
 func loadParams() {
 	flag.StringVar(&filePath, "f", "files/下载1.jpeg", "Please entry file path")
+	flag.BoolVar(&help, "h", false, "Print help info")
 	flag.Parse()
+	if help {
+		usage()
+	}
 }
 
+// print usage info
+func usage() {
+	// print project description info
+	fmt.Fprintf(os.Stderr, `
+		name: oss-example
+		version: v1.0.0
+		Usage: oss-example [-h] -f <upload file path>
+		Options: 
+	`)
+	// print could usage params
+	flag.PrintDefaults()
+}
+
+// upload file
 func upload(filePath string) error {
 	bucket, err := client.Bucket(conf.OssBucket)
 	if err != nil {
 		return err
 	}
 	return bucket.PutObjectFromFile(filePath, filePath)
+}
+
+// download file
+func download(filePath string) error {
+	bucket, err := client.Bucket(conf.OssBucket)
+	downloadURL, err := bucket.SignURL(filePath, oss.HTTPGet, 60*60*24)
+	if err != nil {
+		return err
+	}
+	fmt.Printf("file download url: %s", downloadURL)
+	return nil
 }
 
 func main() {
